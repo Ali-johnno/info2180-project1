167a177c261acaa396eb2ad1b0511a870f814eef
Ali-johnno <ajay051701@gmail.com>
2020-10-22 00:07:00 -0500

    Updated css, html, js

--- info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+"use strict"
+document.addEventListener("DOMContentLoaded", function(){
+    const submit = document.getElementsByTagName('button')[0];
+    const email = document.getElementById('email');
+    const message = document.getElementsByClassName('message')[0];
+    submit.addEventListener('click', function(e){
+        console.log(email.value);
+        e.preventDefault();
+        if (email.value == ''){
+            message.innerHTML = 'Please enter a valid email address'
+        }
+        else { 
+            message.innerHTML = `Thank you! Your email address ${email.value} has been added to our mailing list!`;
+        }
+    });
+});
